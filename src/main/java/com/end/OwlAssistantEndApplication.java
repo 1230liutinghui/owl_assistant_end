78dd26067908a0e102d1d415b88bff37f38d1b38
lth888 <3214067981@qq.com>
2022-06-06 14:52:16 +0800

    第一次提交，创建项目

---
+++ src/main/java/com/end/OwlAssistantEndApplication.java
@@ -0,0 +1,13 @@
+package com.end;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OwlAssistantEndApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OwlAssistantEndApplication.class, args);
+    }
+
+}
